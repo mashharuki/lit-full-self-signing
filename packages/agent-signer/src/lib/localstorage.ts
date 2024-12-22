d0935d065af1c03c1acfd7a14ee7dac3537d2989
Wyatt Barnes <me@wyatt.email>
2024-12-21 22:55:09 -1000

    Remove committed storages

--- packages/agent-signer/src/lib/localstorage.ts
+++ packages/agent-signer/src/lib/localstorage.ts
@@ -1,7 +1,7 @@
 import { LocalStorage } from 'node-localstorage';
 
 // Initialize local storage
-const localStorage = new LocalStorage('./agent-signer-storage');
+const localStorage = new LocalStorage('./.agent-signer-storage');
 
 // Use localStorage directly in your code
 export { localStorage };
